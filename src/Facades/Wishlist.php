5daefef60f99d1735648c5ac93fd5d5ca776042e
Muhammed Sari <muhammed@dive.be>
2021-09-01 14:46:45 +0200

    Add type hints to facade

--- src/Facades/Wishlist.php
+++ src/Facades/Wishlist.php
@@ -4,6 +4,18 @@ namespace Dive\Wishlist\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Dive\Wishlist\Wish add(\Dive\Wishlist\Contracts\Wishable $wishable)
+ * @method static \Dive\Wishlist\WishCollection all()
+ * @method static int count()
+ * @method static bool has(\Dive\Wishlist\Contracts\Wishable $wishable)
+ * @method static bool isEmpty()
+ * @method static bool isNotEmpty()
+ * @method static int purge()
+ * @method static bool remove(int|string|\Dive\Wishlist\Contracts\Wishable $id)
+ *
+ * @see \Dive\Wishlist\WishlistManager
+ */
 class Wishlist extends Facade
 {
     protected static function getFacadeAccessor()
