433db7ed8f0950720985688d1a5842fdbcbebd3b
Muhammed Sari <muhammed@dive.be>
2021-09-01 11:55:57 +0200

    Move helpers to Support

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,7 +1,9 @@
 <?php
 
+use Dive\Wishlist\WishlistManager;
+
 if (! function_exists('wishlist')) {
-    function wishlist()
+    function wishlist(): WishlistManager
     {
         return app(__FUNCTION__);
     }
