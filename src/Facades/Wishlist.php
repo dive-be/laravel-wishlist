959daf787c45be8a93cc364433325428d85d3e55
Muhammed Sari <muhammed@dive.be>
2021-09-05 20:56:34 +0200

    Add extend to doc blocks

--- src/Facades/Wishlist.php
+++ src/Facades/Wishlist.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Dive\Wishlist\Wish add(\Dive\Wishlist\Contracts\Wishable $wishable)
  * @method static \Dive\Wishlist\WishCollection all()
  * @method static int count()
+ * @method static \Dive\Wishlist\WishlistManager extend(string $driver, \Closure $callback)
  * @method static \Dive\Wishlist\EloquentWishlist forUser(\Illuminate\Contracts\Auth\Authenticatable $user)
  * @method static \Dive\Wishlist\Wish|null find(string|\Dive\Wishlist\Contracts\Wishable $id)
  * @method static bool has(\Dive\Wishlist\Contracts\Wishable $wishable)
